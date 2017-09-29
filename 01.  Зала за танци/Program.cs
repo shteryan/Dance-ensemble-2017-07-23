8983b4c70ba171b4d0389bf9059350a034802c7a
shteryan <shter.mladenov@gmail.com>
2017-09-29 20:04:04 +0300

    Removed the comments in choreography section

--- 01.  Зала за танци/Program.cs
+++ 01.  Зала за танци/Program.cs
@@ -9,35 +9,26 @@ namespace _01.Зала_за_танци
     class Program
     {
         static void Main(string[] args)
-        {
-            //Take the parameters for the hall
-            //1.L – дължина на залата в метри – реално число в интервала[10.00 … 100.00];
-            //2.W – ширина на залата в метри – реално число в интервала[10.00 … 100.00];
-            //3.А – страна на гардероба в метри – реално число в интервала[2.00… 20.00];
+        {            
             double hallLenght = double.Parse(Console.ReadLine());
             double hallWidth = double.Parse(Console.ReadLine());
             double wardrobeSide = double.Parse(Console.ReadLine());
-
-            //Set up variables for the calculations
+            
             double hallLenghtCM = hallLenght * 100;
             double hallWidhtCM = hallWidth * 100;
             double wardrobeSideCM = wardrobeSide * 100;
             double dancerSpaceHeldCMSq = 40;
             double spaceForFreeDancingCMSq = 7000;
-
-            //Calculate the area of the hall and stuff
+            
             double hallAreaCMSq = hallLenghtCM * hallWidhtCM;
             double benchAreaCMSq = hallAreaCMSq / 10;
             double wardrobeAreaCMSq = wardrobeSideCM * wardrobeSideCM;
-
-            //Calculate the free space left
+            
 
             double hallFreeArea = hallAreaCMSq - (wardrobeAreaCMSq + benchAreaCMSq);
-
-            //Calculate the result
+            
             int numberOfDancers = (int)(hallFreeArea / (dancerSpaceHeldCMSq + spaceForFreeDancingCMSq));
-
-            //Print the result
+                        
             Console.WriteLine(numberOfDancers);
         }
     }
--- 02. Хореография/Program.cs
+++ 02. Хореография/Program.cs
@@ -9,18 +9,15 @@ namespace _02.Хореография
     class Program
     {
         static void Main(string[] args)
-        {
-            //Take the info from the user            
+        {                        
             double numberOfSteps = double.Parse(Console.ReadLine());
             double numberOfDancers = double.Parse(Console.ReadLine());
             double numberOfDaysToPractice = double.Parse(Console.ReadLine());
-
-            //Set up the variables for the calculations            
+                                   
             double stepsForADay = Math.Ceiling(100 * ((numberOfSteps / numberOfDaysToPractice) / numberOfSteps));
             bool thereIsEnoughTime = stepsForADay <= 13;
             double percentOfStepsPerDancer = stepsForADay / numberOfDancers;
-
-            //Logical checks for printing a result
+                        
             if (thereIsEnoughTime)
             {
                 Console.WriteLine($"Yes, they will succeed in that goal! {percentOfStepsPerDancer:f2}%.");
@@ -29,7 +26,6 @@ namespace _02.Хореография
             {
                 Console.WriteLine($"No, They will not succeed in that goal! Required {percentOfStepsPerDancer:f2}% steps to be learned per day.");
             }
-
         }
     }
 }
